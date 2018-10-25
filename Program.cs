7175910b6ed5f60a4fb6427c1c225f527ee79e4c
Vadim0108 <kazakoff.vadik@yandex.ru>
2018-10-25 15:43:13 +0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+            int pi;
+            pi = a + b;
+            Console.WriteLine(pi);
+
+        }
+        // Виета
+        // x^2+px+q=0
+        // x1+x2=-p
+        // x1*x2=q
+    }
+}
